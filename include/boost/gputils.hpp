e8512a3c74aa34af2437ea3f0ce35509f880b854
Matt Borland <matt@mattborland.com>
2024-08-07 17:08:37 -0400

    Add convenience header

---
+++ include/boost/gputils.hpp
@@ -0,0 +1,12 @@
+//  Copyright (c) 2024 Matt Borland
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_GPUTILS_HPP
+#define BOOST_GPUTILS_HPP
+
+#include <boost/gputils/config.hpp>
+#include <boost/gputils/type_traits.hpp>
+
+#endif // BOOST_GPUTILS_HPP
